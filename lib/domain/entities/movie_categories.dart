02147003633bc5d2a3d2e3cccfaf990a4d5e6a67
Thiago Fontes <13390191+ThiagoFontes@users.noreply.github.com>
2020-10-24 17:16:24 -0300

    Creating base repositories usecases and defining entities and models

---
+++ lib/domain/entities/movie_categories.dart
@@ -0,0 +1,6 @@
+enum MovieCategories {
+  acao,
+  aventura,
+  fantasia,
+  comedia,
+}
---
+++ lib/domain/usecases/movie_list_category_usecase.dart
@@ -0,0 +1,8 @@
+import 'package:movie_app/domain/entities/movie_categories.dart';
+
+class MovieListCategoryUsecase {
+
+  call({MovieCategories movieCategory}) {
+    
+  }
+}
