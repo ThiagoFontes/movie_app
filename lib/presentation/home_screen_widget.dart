c77e70ffa683a7ff4fad8c70596fe325ec7230dd
Thiago Fontes <13390191+ThiagoFontes@users.noreply.github.com>
2020-10-24 16:07:19 -0300

    Adding dependencies flutter_bloc and equatable

---
+++ lib/presentation/home_screen_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class HomeScreenWidget extends StatefulWidget {
+  HomeScreenWidget({Key key}) : super(key: key);
+
+  @override
+  _HomeScreenWidgetState createState() => _HomeScreenWidgetState();
+}
+
+class _HomeScreenWidgetState extends State<HomeScreenWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
